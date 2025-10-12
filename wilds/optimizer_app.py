e4042edf0d2d976bb7ab7289e398f9e93f2be7e9
Yoann Bernard <yoann@get-potions.com>
2025-10-12 17:19:56 +0200

    Add weapon input + async opti + better style

--- wilds/optimizer_app.py
+++ wilds/optimizer_app.py
@@ -4,63 +4,73 @@ from opti_wilds import load_data_files, define_data, run_optimizer, output_build
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/run', methods=['POST'])
+def run_optimization():
     items_data_default, decorations_default, available_skills, available_sets = load_data_files()
 
-    if request.method == 'POST':
-        try:
-            desired_skills = []
-            traversed_skills_idx = []
-            desired_sets = []
-            traversed_sets_idx = []
-            for key, value in request.form.items():
-                if key.startswith('skills_') and (i := int(key.split('_')[-1])) not in traversed_skills_idx:
-                    name = request.form[f'skills_name_{i}']
-                    max_points = int(request.form.get(f'skills_max_{i}', 5))
-                    weight = int(request.form.get(f'skills_weight_{i}', 10))
-                    traversed_skills_idx.append(i)
-                    if name:
-                        desired_skills.append({'name': name, 'max_points': max_points, 'weight': weight})
-                elif key.startswith('sets_') and (i := int(key.split('_')[-1])) not in traversed_sets_idx:
-                    name = request.form[f'sets_name_{i}']
-                    min_pieces = int(request.form.get(f'sets_min_{i}', 2))
-                    traversed_sets_idx.append(i)
-                    if name:
-                        desired_sets.append({'set': name, 'min_pieces': min_pieces})
+    desired_skills = []
+    traversed_skills_idx = []
+    desired_sets = []
+    traversed_sets_idx = []
+    desired_weapons = []
+    traversed_weapons_idx = []
+
+    for key, value in request.form.items():
+        if key.startswith('skills_') and (i := int(key.split('_')[-1])) not in traversed_skills_idx:
+            name = request.form[f'skills_name_{i}']
+            max_points = int(request.form.get(f'skills_max_{i}', 5))
+            weight = int(request.form.get(f'skills_weight_{i}', 10))
+            traversed_skills_idx.append(i)
+            if name:
+                desired_skills.append({'name': name, 'max_points': max_points, 'weight': weight})
+        elif key.startswith('sets_') and (i := int(key.split('_')[-1])) not in traversed_sets_idx:
+            name = request.form[f'sets_name_{i}']
+            min_pieces = int(request.form.get(f'sets_min_{i}', 2))
+            traversed_sets_idx.append(i)
+            if name:
+                desired_sets.append({'set': name, 'min_pieces': min_pieces})
+        elif key.startswith('weapons_') and (i := int(key.split('_')[-1])) not in traversed_weapons_idx:
+            name = request.form[f'weapons_name_{i}']
+            traversed_weapons_idx.append(i)
+            if name:
+                desired_weapons.append(name)
 
 
-            # Filter items_data
-            filter_items = request.form.get('filter_items', '')
-            items_data_input = {k: v for k, v in items_data_default.items() if not filter_items or filter_items.lower() in k.lower()}
+    # Filter items_data
+    filter_items = request.form.get('filter_items', '')
+    filtered_items_data = {k: v for k, v in items_data_default.items() if not filter_items or filter_items.lower() in k.lower()}
 
-            # todo : change with selected weapon
-            items_data_input['weapon'] = [item for item in items_data_input['weapon'] if 'Dimensius' == item['name']]
+    # Filter weapons
+    filtered_items_data['weapon'] = [item for item in filtered_items_data['weapon'] if item['name'] in desired_weapons]
 
-            # Filter decorations
-            filter_decos = request.form.get('filter_decos', '')
-            decorations_filtered = [d for d in decorations_default if not filter_decos or filter_decos.lower() in d['name'].lower()]
+    # Filter decorations
+    filter_decos = request.form.get('filter_decos', '')
+    filtered_decorations = [d for d in decorations_default if not filter_decos or filter_decos.lower() in d['name'].lower()]
 
-            N = int(request.form['N'])
+    N = min(int(request.form['N']), 20)
 
-            data = define_data(desired_skills, desired_sets, items_data_input, decorations_filtered, include_all_amulets=request.form.get('include_all_amulets', ''))
+    data = define_data(desired_skills, desired_sets, filtered_items_data, filtered_decorations, include_all_amulets=request.form.get('include_all_amulets', ''))
 
-            builds = run_optimizer(data, N)
+    builds = run_optimizer(data, N)
 
-            # Format builds
-            output = output_builds(builds, data, next_line="<br>")
+    return output_builds(builds, data, next_line="<br>")
 
-            return render_template('result.html', output=output)
+@app.route('/', methods=['GET'])
+def index():
+    items_data_default, _, available_skills, available_sets = load_data_files()
 
-        except Exception as e:
-            return f"Error: {str(e)}"
+    available_weapons = list(map(lambda x: x['name'], items_data_default['weapon']))
 
-    return render_template('form.html', available_skills=available_skills, available_sets=available_sets)
+    return render_template(
+        'form.html',
+        available_skills=available_skills,
+        available_sets=available_sets,
+        available_weapons=available_weapons
+    )
 
 @app.route('/static/<path:path>', methods=['GET'])
 def send_static(path):
     return send_from_directory('static', path)
 
 if __name__ == '__main__':
-    # app.run(host='127.0.0.1', debug=True)
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.1', debug=True)
--- wilds/static/main.js
+++ wilds/static/main.js
@@ -14,13 +14,27 @@ document.addEventListener('DOMContentLoaded', () => {
         highlightTyped: true,
         onSelectItem: onSetSelect
     });
+
+    Autocomplete.init('#weapons-input', {
+        items: weaponsData.map(e => ({label: e, value: e})),
+        suggestionsThreshold: 0,
+        highlightTyped: true,
+        onSelectItem: onWeaponSelect
+    });
 });
 
 let skillIndex = 0;
 let setIndex = 0;
+let weaponIndex = 0;
 const skillsInput = document.getElementById('skills-input')
 const setsInput = document.getElementById('sets-input')
+const weaponsInput = document.getElementById('weapons-input')
 
+const form = document.querySelector('form');
+const submitLaunchButton = document.getElementById('submit-launch');
+
+const loader = document.getElementById('loader');
+const optimizationResultsContainer = document.getElementById('optimization-results');
 
 //  defaults TESTING
 Array.from([
@@ -53,28 +67,56 @@ Array.from([
     addBadge('sets-badges', e.set, setIndex++, 'Min Pieces', null, e.min_pieces, '')
 });
 
+const idx = weaponsData.indexOf('Dimensius');
+if (idx > -1) {
+    weaponsData.splice(idx, 1);
+    addBadge('weapons-badges', 'Dimensius', weaponIndex++, null, null, '', '')
+}
+// end defaults
+
 function refreshAutocompleteData(input, data) {
     Autocomplete.getInstance(input).setData(data.map(e => ({label: e, value: e})));
     input.value = '';
 }
 
 function addBadge(containerId, name, index, label1, label2, default1, default2) {
-    const isSet = label2 === null;
+    const noFirstLabel = label1 === null;
+    const noSecondLabel = label2 === null;
     const container = document.getElementById(containerId);
     const badge = document.createElement('span');
-    badge.className = 'badge bg-primary me-2 mb-2';
+    badge.className = 'badge bg-secondary me-2 mb-2 d-flex justify-content-between';
+    badge.style.width = '30%';
     badge.innerHTML = `
-                ${name}
-                <input type="hidden" name="${containerId.split('-')[0]}_name_${index}" value="${name}">
-                <input type="number" name="${containerId.split('-')[0]}_${isSet ? 'min' : 'max'}_${index}" value="${default1}" placeholder="${label1}" size="4">
-                <input type="${isSet ? 'hidden' : 'number'}" name="${containerId.split('-')[0]}_weight_${index}" value="${default2}" placeholder="${label2}" size="4">
-                <button type="button" class="btn-close btn-close-white ms-2" aria-label="Remove"></button>
+                <div class="d-flex align-items-center">
+                    ${name}
+                </div>
+                <div class="d-flex justify-content-end align-items-center">
+                    <input type="hidden" name="${containerId.split('-')[0]}_name_${index}" value="${name}">
+                    <input 
+                        class="ms-1 form-control p-1"
+                        style="width:3em;height:1.75em" 
+                        type="${noFirstLabel ? 'hidden' : 'number'}" 
+                        name="${containerId.split('-')[0]}_${noSecondLabel ? 'min' : 'max'}_${index}" 
+                        value="${default1}" 
+                        title="${label1}">
+                    <input 
+                        class="ms-1 form-control p-1"
+                        style="width:3em;height:1.75em" 
+                        type="${noSecondLabel ? 'hidden' : 'number'}"
+                        name="${containerId.split('-')[0]}_weight_${index}"
+                        value="${default2}"
+                        title="${label2}">
+                    <button type="button" class="btn-close btn-close-white ms-2" aria-label="Remove"></button>
+                </div>
             `;
     container.appendChild(badge);
 
     badge.querySelector('.btn-close').addEventListener('click', () => {
         badge.remove();
-        if (isSet) {
+        if (noFirstLabel) {
+            weaponsData.push(name);
+            refreshAutocompleteData(weaponsInput, weaponsData)
+        } else if (noSecondLabel) {
             setsData.push(name);
             refreshAutocompleteData(setsInput, setsData)
         } else {
@@ -84,7 +126,7 @@ function addBadge(containerId, name, index, label1, label2, default1, default2) 
     });
 }
 
-function onSkillSelect({ value }) {
+function onSkillSelect({value}) {
     const idx = skillsData.indexOf(value);
     if (idx === -1) return;
     skillsData.splice(idx, 1);
@@ -93,7 +135,7 @@ function onSkillSelect({ value }) {
     skillIndex++;
 }
 
-function onSetSelect({ value }) {
+function onSetSelect({value}) {
     const idx = setsData.indexOf(value);
     if (idx === -1) return;
     setsData.splice(idx, 1);
@@ -101,3 +143,37 @@ function onSetSelect({ value }) {
     addBadge('sets-badges', value, setIndex, 'Min Pieces', null, 2, '');
     setIndex++;
 }
+
+function onWeaponSelect({value}) {
+    const idx = weaponsData.indexOf(value);
+    if (idx === -1) return;
+    weaponsData.splice(idx, 1);
+    refreshAutocompleteData(weaponsInput, weaponsData);
+    addBadge('weapons-badges', value, weaponIndex, null, null, '', '');
+    weaponIndex++;
+}
+
+form.addEventListener('submit', (e) => {
+    submitLaunchButton.disabled = true;
+    loader.style.display = 'block';
+    optimizationResultsContainer.innerHTML = '';
+
+
+    e.stopPropagation();
+    e.preventDefault();
+    const formData = new FormData(form);
+    fetch(runUrl, {
+        "method": "POST",
+        "body": formData,
+    })
+        .then(response => response.json())
+        .then(data => {
+            loader.style.display = 'none';
+            data.forEach(e => {
+                const el = document.createElement('pre');
+                el.innerHTML = e;
+                optimizationResultsContainer.appendChild(el);
+            });
+            submitLaunchButton.disabled = false;
+        })
+})
